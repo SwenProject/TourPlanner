884a6eb0ab11539feb21dff16a99dd546c322fd8
JAJA <101059876+jajabee@users.noreply.github.com>
2023-04-24 11:38:15 +0200

    updated CSS, added shadows and logo

--- src/main/java/com/tourplanner/TourPlannerApp.java
+++ src/main/java/com/tourplanner/TourPlannerApp.java
@@ -3,6 +3,7 @@ package com.tourplanner;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -13,9 +14,11 @@ public class TourPlannerApp extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(TourPlannerApp.class.getResource("views/main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-//        Scene scene = new Scene(fxmlLoader.load(), 700, 700);
+//      Scene scene = new Scene(fxmlLoader.load(), 700, 700);
         //scene.setFill(Color.TRANSPARENT);
         stage.initStyle(StageStyle.DECORATED);
+//      stage.getIcons().add(new Image("https://genuinecoder.com/wp-content/uploads/2022/06/genuine_coder-3.png"));
+        stage.getIcons().add(new Image(TourPlannerApp.class.getResourceAsStream("images/iconicon.png")));
         stage.setTitle("Tour Planner");
         stage.setScene(scene);
         //maximize window
--- src/main/java/com/tourplanner/controller/MainController.java
+++ src/main/java/com/tourplanner/controller/MainController.java
@@ -2,9 +2,11 @@ package com.tourplanner.controller;
 
 import javafx.scene.control.SplitPane;
 import javafx.scene.layout.*;
+
 public class MainController {
 
     public SplitPane mainContainer;
+
     public VBox leftContainer;
 
     public SplitPane rightContainer;
