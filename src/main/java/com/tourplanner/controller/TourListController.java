2a2ac83f1c8ddfb0fae9b49f9084254f4b128a77
FlyingWalross <jakob.jandl@gmail.com>
2023-05-12 02:48:12 +0200

    added icon for tour list when there are no tours to display

--- src/main/java/com/tourplanner/controller/TourListController.java
+++ src/main/java/com/tourplanner/controller/TourListController.java
@@ -2,15 +2,14 @@ package com.tourplanner.controller;
 
 import com.tourplanner.logic.TourLogic;
 import com.tourplanner.models.Tour;
-import javafx.event.ActionEvent;
-import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.layout.VBox;
 import javafx.util.Callback;
 
 public class TourListController {
     private final TourLogic tourLogic;
-    public Label noToursLabel;
+    public VBox noToursContainer;
     public ListView<Tour> tourList;
 
     public TourListController(TourLogic tourLogic) {
@@ -24,8 +23,8 @@ public class TourListController {
         tourList.managedProperty().bind(tourLogic.getSearchedToursListProperty().emptyProperty().not());
 
         //bind visibility of noToursLabel to whether or not the list is empty
-        noToursLabel.visibleProperty().bind(tourLogic.getSearchedToursListProperty().emptyProperty());
-        noToursLabel.managedProperty().bind(tourLogic.getSearchedToursListProperty().emptyProperty());
+        noToursContainer.visibleProperty().bind(tourLogic.getSearchedToursListProperty().emptyProperty());
+        noToursContainer.managedProperty().bind(tourLogic.getSearchedToursListProperty().emptyProperty());
 
         tourList.setItems(tourLogic.getSearchedToursList());
         tourList.setCellFactory(new Callback<ListView<Tour>, ListCell<Tour>>() {
